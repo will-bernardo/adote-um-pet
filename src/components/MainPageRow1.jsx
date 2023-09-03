31da4dd2adc23618345021603c79fb10d6459350
William Bernardo da Silva <bernardowill175@gmail.com>
2023-09-03 14:44:08 -0300

    Update MainPageRow1.jsx

--- src/components/MainPageRow1.jsx
+++ src/components/MainPageRow1.jsx
@@ -2,8 +2,11 @@ import { Grid, Image, Button } from "@chakra-ui/react";
 
 export default function MainPageRow1() {
   return (
-    <Grid minHeight='100vh' minWidth='100vh' bg='#f3f4f6' p='2rem 12%'>
-      <Image src='./images/cachorro_na_caixa.png' alt='' w='100%'/>
+    <Grid 
+    templateColumns='repeat(2, 1fr)' 
+    templateRows='repeat(2,1fr)'
+    minHeight='100vh' minWidth='100vh' bg='#f3f4f6' p='2rem 12%'>
+      <Image colStart={1} colEnd={3} src='./images/cachorro_na_caixa.png' alt='' w='100%'/>
       <Button colorScheme='facebook' variant='solid'>Quero adotar</Button>
       <Button colorScheme='facebook' variant='outline'>Quero ajudar</Button>
     </Grid>
