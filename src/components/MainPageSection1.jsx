127ecfce4b88ff52b171d0ee2724b86c0d5112c3
will-bernardo <bernardowill175@gmail.com>
2023-12-06 16:46:15 -0300

    fixed page scale on browser zoom

--- src/components/MainPageSection1.jsx
+++ src/components/MainPageSection1.jsx
@@ -3,9 +3,9 @@ import { Flex, Image, Button } from "@chakra-ui/react";
 export default function MainPageSection1() {
 
   return (
-    <Flex as='section' flexDir='column' gap='2.25rem' h='90vh' minWidth='100vh' bg='#f3f4f6' pt='2rem' px='14%'>
-      <Image src='./images/cachorro_na_caixa.png' alt='' w='100%'/>
-      <Flex justify='center' gap='1rem'>
+    <Flex as='section' flexDir='column' align='center' gap='2.25rem' pb='8' minWidth='100vh' bg='#f3f4f6' pt='2rem'>
+      <Image src='./images/cachorro_na_caixa.png' alt='' maxW='61rem' />
+      <Flex justify='center' gap='1rem' w='61rem'>
         <Button colorScheme='facebook' variant='solid' w='50%'>Quero adotar</Button>
         <Button colorScheme='facebook' variant='outline' w='50%' >Quero ajudar</Button>
       </Flex>
--- src/components/MainPageSection4.jsx
+++ src/components/MainPageSection4.jsx
@@ -3,20 +3,19 @@ import { Flex, Image, Text, Spacer } from "@chakra-ui/react";
 export default function MainPageSection2() {
   return (
     <Flex as='section' bg='#f3f4f6' p='4rem 0'>
-      <Flex bg='#F5DAC2' h='26rem' w='100%' p='14%' justify='center' align='center'>
-        <Flex align='center' justify='center' border='1px' borderColor='#004569' borderRadius='3px' bg='' h='27rem' w='42%' p='0 0.75rem'>
-          <Image src='./images/depoimento.png' alt='' w='95%'/>
+      <Flex bg='#F5DAC2' h='26rem' gap='2rem' w='100%' justify='center' align='center'>
+        <Flex align='center' justify='center' border='1px' borderColor='#004569' borderRadius='3px' bg='' h='27rem' w='25.5rem' p='0 0.75rem'>
+          <Image src='./images/depoimento.png' alt='' w='97%' />
         </Flex>
-      <Spacer />
-        <Flex align='center' flexDir='column' gap='0.25rem' w='54%'>
+        <Flex align='center' flexDir='column' gap='0.25rem' w='34rem'>
           <Flex alignSelf='end' >
             <Stars />
-            </Flex>
+          </Flex>
           <Image alignSelf='start' src='./open-quote.svg' alt='' w='2rem' />
           <Text textAlign='center' w='94%' fontSize='18pt' color='#2a2a2a'>
-          Adotar a mel foi a melhor coisa que poderia ter feito, 
-          dar amor e carinho a um animal e ser retribuida 
-          é sem igual.
+            Adotar a mel foi a melhor coisa que poderia ter feito,
+            dar amor e carinho a um animal e ser retribuida
+            é sem igual.
           </Text>
           <Image alignSelf='end' src='./close-quote.svg' alt='' w='2rem' />
           <Text as='i' fontSize='16pt' alignSelf='start'>- Ana Clara</Text>
@@ -29,7 +28,7 @@ export default function MainPageSection2() {
 
 function Stars() {
   return (
-    <Flex>  
+    <Flex>
       <Image src='./star.svg' alt='' />
       <Image src='./star.svg' alt='' />
       <Image src='./star.svg' alt='' />
